e7a31a869bb66b196035013ff2d69c71ce4a076d
i'mfebri <iamfebriadji@gmail.com>
2021-03-27 21:49:40 +0700

    update controllers & route articles

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,11 +1,14 @@
 'use strict'
 
-const conn = require('../config/db-conn')
-const formidable = require('formidable')
+const 
+  conn = require('../config/db-conn'),
+  formidable = require('formidable'),
+  fs = require('fs'),
+  crypto = require('crypto')
 
 // get the structure of the json article
 exports.getArticles = async function(req, res, next) {
-  let sql = await `SELECT * FROM articles`
+  let sql = await `SELECT * FROM articles ORDER BY created_at DESC`
 
   conn.query(sql, (err, rows) => {
     if (err) return console.log(err)
@@ -28,12 +31,82 @@ const formUpload = (request) => new Promise((resolve, reject) => {
   })
 })
 
+const fileSizeConvert = (bytes) => new Promise((resolve, reject) => {
+  let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes == 0) return resolve('n/a')
+
+  let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+  if (i == 0) return resolve(bytes + " " + sizes[i])
+
+  resolve((bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i])
+})
+
 exports.postArticles = async function(req, res, next) {
   try {
-    let form = await formUpload(req)
-    res.json(form)
+    const form = await formUpload(req)
+    const fileSize = await fileSizeConvert(form.file.size)
+
+    // create new file name
+    const 
+      createfileName = crypto.randomBytes(10).toString('hex'),
+      newFileName = createfileName + '.md'
+
+    const url = form.title.split('')
+    for ( let i in url ) if (url[i] == ' ' ) url[i] = '-'
+
+    // sql query
+    let sql = `INSERT INTO articles ( title, description, url, file_name, file_size )
+      VALUES ( '${form.title}', '${form.description}', '${url.join('').toLowerCase()}', '${newFileName}', '${fileSize}' )
+    `
+    conn.query(sql, err => {
+      if (err) res.status(400).json({
+        status: 'failed',
+        code: 400,
+        message: 'failed to add article'
+      })
+
+      if (!fs.existsSync('uploads/')) fs.mkdirSync('uploads/')
+
+      const readfile = new Promise(resolve => {
+        let file = fs.readFileSync(form.file.path)
+
+        resolve(file)
+      })
+
+      async function writeFile() {
+        let file = await readfile
+
+        fs.writeFile('uploads/' + newFileName, file, 'utf-8', err => {
+          if (err) return console.log(err)
+
+          res.status(200).json({
+            status: 'success',
+            code: 200,
+            message: 'managed to add to the article'
+          })
+        })
+      }
+
+      writeFile()
+    })
   }
   catch(err) {
     console.log(err)
   }
+}
+
+exports.deleteArticles = async function(req, res, next) {
+  let 
+    params = req.params.id,
+    sql = await `DELETE FROM articles WHERE id = ${params}`
+
+  conn.query(sql, err => {
+    if (err) return console.log(err)
+
+    res.status(200).json({
+      status: 'success',
+      code: 200,
+      message: 'successfully deleted the article'
+    })
+  })
 }
--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const router = require('express').Router()
-const { getArticles, postArticles } = require('../controllers/articles')
+const { getArticles, postArticles, deleteArticles } = require('../controllers/articles')
 
 router.get('/', getArticles)
 router.post('/', postArticles)
+router.delete('/:id', deleteArticles)
 
 module.exports = router
